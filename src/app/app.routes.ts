fac1ebd7c54f5c631899ce231e768d99eb189327
Priyank - PC <priyank.patadiya.3@gmail.com>
2024-10-07 04:23:27 -0400

    Add upcoming component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AddExpenseComponent } from './expenses/add-expense/add-expense.component';
 import { ExpensesComponent } from './expenses/expenses.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { UpcomingFeaturesComponent } from './upcoming-features/upcoming-features.component';
 
 export const routes: Routes = [
     {
@@ -18,6 +19,11 @@ export const routes: Routes = [
         path: 'edit-expense/:id',
         component: AddExpenseComponent,
         title: 'Edit Expense'
+    },
+    {
+        path: 'upcoming-features',
+        component: UpcomingFeaturesComponent,
+        title: 'Next Version'
     },
     {
         path: '**',
---
+++ src/app/upcoming-features/upcoming-features.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upcoming-features',
+  standalone: true,
+  imports: [],
+  templateUrl: './upcoming-features.component.html',
+  styleUrl: './upcoming-features.component.css'
+})
+export class UpcomingFeaturesComponent {
+
+}
